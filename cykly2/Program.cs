16b8ca76ac689dec1b0a26b17145c8ae3112e041
Jana <s2022BockovaJana@skolabaltaci.cz>
2024-01-25 19:49:08 +0100

    Přidejte soubory projektu.

---
+++ cykly2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cykly2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //průměr sudých čísel
+            Console.WriteLine("Kolik chcete zadat čísel??");
+            int cisla = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Zadejte čísla");
+            int pocet_cisel = 0;
+            int sude = 0;
+            for (int i = 0; i < cisla; i++)
+            {
+                int b = Convert.ToInt16(Console.ReadLine());
+                if (b % 2 == 0)
+                {
+                    sude += b;
+                    pocet_cisel++;
+                }
+
+            }
+            Console.WriteLine("Průměr sudých čisel je " + sude / pocet_cisel);
+
+            Console.ReadKey();
+        }
+    }
+}
